5e1e90d4eb15295724d77fa7ee0541a650c4f03c
Dalia <dalia2000w@gmail.com>
2024-03-26 10:13:43 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import QRCode from "qrcode.react";
+export default function App() {
+  return (
+    <div style={{ padding: 25 , marginTop: 200, display: "flex", flexDirection: "row" }}>
+      <div>
+        <QRCode value="https://www.google.com/" style={{ marginRight: 40 }} />
+        <p>Google</p>
+      </div>
+      <div>
+        <QRCode value="https://github.com/" style={{ marginRight: 40 }} />
+        <p>Github</p>
+      </div>
+      <div>
+        <QRCode value="https://www.youtube.com/" style={{ marginRight: 40 }} />
+        <p>YouTube</p>
+      </div>
+      <div>
+        <QRCode value="https://www.ndu.edu.lb/" style={{ marginRight: 40 }} />
+        <p>NDU Website</p>
+      </div>
+      <div>
+        <QRCode
+          value="https://www.netflix.com/lb-en/" style={{ marginRight: 40 }} />
+        <p>Netflix</p>
+      </div>
+      <div>
+        <QRCode value="https://www.tinkercad.com/" style={{ marginRight: 40 }} />
+        <p>Tinkercad</p>
+      </div>
+      <div>
+        <QRCode value="https://www.linkedin.com/" style={{ marginRight: 40 }} />
+        <p>Linkedin</p>
+      </div>
+    </div>
+  );
+}
